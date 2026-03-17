7033617b742e8e5e9135dac5d614cb97d9f0d0b5
jasurbek <131148178+jasurbek2772@users.noreply.github.com>
2026-03-17 16:39:42 +0700

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || 'mysql.railway.internal',
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'CyZIdMhYVIkVzeNdvpVOZirDydktQoDu',
+  database: process.env.DB_NAME || 'railway',
   waitForConnections: true,
   connectionLimit: 10
 });
